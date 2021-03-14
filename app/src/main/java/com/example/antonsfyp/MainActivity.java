3a10535e3c65b86aca10cf64e1dcae1b9ec2fed2
supernsansa <supernsansa@gmail.com>
2021-03-15 00:03:28 +0100

    Restructured the adding words process. Split it up into three separate activities. Also, added the logo to the front page.

--- app/src/main/java/com/example/antonsfyp/MainActivity.java
+++ app/src/main/java/com/example/antonsfyp/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    //TODO Redo buttons and colour scheme
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
